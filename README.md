c9250572753974fe2af82609464a4367c9f33f9c
Bernardo Sertório <59669475+bernardosertorio@users.noreply.github.com>
2024-11-22 23:23:37 +0000

    Add prettier scripts  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 repository for training
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>A vida é como andar de bicicleta. Pra se manter em equilíbrio é preciso estar em movimento.</h1>
-} 
+  return (
+    <h1>
+      A vida é como andar de bicicleta. Pra se manter em equilíbrio é preciso
+      estar em movimento.
+    </h1>
+  );
+}
 
-export default Home
+export default Home;
