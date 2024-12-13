326e8b1acc4a6fc525fdefc39ec9ff9d2e1b2379
Bernardo Sertório <59669475+bernardosertorio@users.noreply.github.com>
2024-12-13 23:57:26 +0000

    Refactor database flow and make better to do with errors

--- infra/database.js
+++ infra/database.js
@@ -5,14 +5,22 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+
+    console.log("✅ Query Success!", queryObject);
+
+    return result;
+  } catch (error) {
+    console.error("⛔ Query Error: ", queryObject, error.message);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
