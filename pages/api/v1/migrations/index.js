29cc82ac982fb8b709983d64b948ea604ef46d9f
Bernardo Sertório <ondaaposonda@gmail.com>
2025-01-02 17:03:32 -0300

    Correct version

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,11 +4,11 @@ import database from "infra/database";
 
 export default async function migrations(request, response) {
 
- /* const methodsNotAllowed = ["PATCH", "DELETE", "PUT"]
+  const methodsNotAllowed = ["PATCH", "DELETE", "PUT"]
 
   if (methodsNotAllowed.includes(request.method)) {
-    
-  } */
+    return response.status(405).end();
+  }
 
   const dbClient = await database.getNewClient();
 
@@ -40,6 +40,4 @@ export default async function migrations(request, response) {
 
     return response.status(200).json(migratedMigrations);
   }
-
-  return response.status(405).end();
 }
